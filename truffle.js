cd37610736b07a49d5d36f3c1642bb3002714f03
Pritty Skaria <prittyskaria@gmail.com>
2018-06-02 06:27:32 +0100

    added networks Development and net42

--- truffle.js
+++ truffle.js
@@ -1,4 +1,16 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      gas: 500000,
+      network_id: "*" // Match any network id
+    },
+  net42: {
+      host: "localhost",
+      port: 8545,
+      gas: 500000,
+      network_id: 42 // Match any network id
+    }
+   }
 };
